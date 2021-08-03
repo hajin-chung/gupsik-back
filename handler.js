319c6ddab5757757e31e0888f696736892635fd7
hajin-chung <hajinchung1@gmail.com>
2021-08-03 20:48:14 +0900

    handler.js for serverless

---
+++ handler.js
@@ -0,0 +1,82 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+const cors = require('cors');
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
+
+app.use(cors());
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+let cache = {};
+
+app.get('/meal', async (req, res) => {
+  let now = new Date();
+  let nowMonth = `${now.getMonth()+1}`;
+  let nowDay = `${now.getDate()}`;
+  let { month=nowMonth, day=nowDay } = req.query;
+  console.log({month, day});
+
+  if(month && day) {
+    let searchQuery = `https://www.dimigo.hs.kr/index.php?mid=school_cafeteria&search_target=title`
+      +`&search_keyword=${month}%EC%9B%94%20${day}%EC%9D%BC&page=1`;
+
+    if(cache.hasOwnProperty(`${month}-${day}`)) {
+      res.status(200).json(cache[`${month}-${day}`]);
+    } else {
+      try {
+        let listRes = await fetch(searchQuery);
+        listRes = await listRes.text();
+
+        let $ = cheerio.load(listRes);
+        let mealUrl = $('#dimigo_post_cell_2 > tr:nth-child(1) > td.title > div > a').attr('href');
+        mealUrl = encodeURI(mealUrl);
+        console.log({ searchQuery, mealUrl });
+        
+        let mealRes = await fetch(mealUrl);
+        mealRes = await mealRes.text();
+
+        $ = cheerio.load(mealRes);
+        let meal = {};
+
+        $('#siDoc > ul:nth-child(5) > li > div.scConDoc.clearBar > div > p')
+          .each((i, p) => {
+            let inner = $(p).text();
+            if(inner && inner != undefined && inner.length > 10) {
+              let name = inner.split(' : ')[0];
+              let content = inner.split(' : ')[1].split('/');
+              meal[name] = content;
+            }
+          });
+        
+        cache[`${month}-${day}`] = meal;
+        
+        res.status(200).json(meal);
+      } catch(e) {
+        console.error(e);
+        res.status(500).send({status: 'fail', messsage: e});
+      }
+    }
+  } else {
+    res.status(500).send({status: 'fail', message: 'parameter empty'});
+  }
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+module.exports.handler = serverless(app);
